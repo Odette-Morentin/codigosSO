53edd4f26ab02991c28d32ffb9ec95f4392406da
Odette Morentin <sandraodettems@gmail.com>
2025-11-15 23:48:10 +0100

    Código comentado

--- L1/tarea2.c
+++ L1/tarea2.c
@@ -18,22 +18,22 @@ int i;
 struct stat atributos;
 char tipoArchivo[30];
 
-if(argc<2) {
+if(argc<2) { //Revisa que se hayan pasado argumentos
 	printf("\nSintaxis de ejecucion: tarea2 [<nombre_archivo>]+\n\n");
 	exit(-1);
 }
-for(i=1;i<argc;i++) {
+for(i=1;i<argc;i++) { //Recorre los parámetros que se le pasaron
 	printf("%s: ", argv[i]);
 	if(lstat(argv[i],&atributos) < 0) {
 		printf("\nError al intentar acceder a los atributos de %s",argv[i]);
 		perror("\nError en lstat");
 	}
-	else {
+	else { //Explica qué tipo es
 		if(S_ISREG(atributos.st_mode)) strcpy(tipoArchivo,"Regular");
 		else if(S_ISDIR(atributos.st_mode)) strcpy(tipoArchivo,"Directorio");
 		else if(S_ISCHR(atributos.st_mode)) strcpy(tipoArchivo,"Especial de caracteres");
 		else if(S_ISBLK(atributos.st_mode)) strcpy(tipoArchivo,"Especial de bloques");
-		else if(S_ISFIFO(atributos.st_mode)) strcpy(tipoArchivo,"Tuber�a con nombre (FIFO)");
+		else if(S_ISFIFO(atributos.st_mode)) strcpy(tipoArchivo,"Tuber�a con nombre (FIFO)");
 		else if(S_ISLNK(atributos.st_mode)) strcpy(tipoArchivo,"Enlace relativo (soft)");
 		else if(S_ISSOCK(atributos.st_mode)) strcpy(tipoArchivo,"Socket");
 		else strcpy(tipoArchivo,"Tipo de archivo desconocido");
